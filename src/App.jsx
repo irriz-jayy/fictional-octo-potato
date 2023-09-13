134ddf6bda091a912d065dc24b7721649717af77
irriz-jayy <jayfelix789@gmail.com>
2023-09-13 12:10:11 +0300

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<Homepage />} />
       </Routes>
