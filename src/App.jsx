0c0371d47477e4f021b1387e461283f6d940e7d4
irriz-jayy <jayfelix789@gmail.com>
2023-09-13 12:26:17 +0300

    added save page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Homepage from "./pages/Homepage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Create from "./pages/Create";
+import Saved from "./pages/Saved";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route path="/create" element={<Create />} />
+        <Route path="/saved" element={<Saved />} />
       </Routes>
       <Footer />
     </>
