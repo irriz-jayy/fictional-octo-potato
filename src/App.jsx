854140be6541286ca904ad4034a66b659e392eca
irriz-jayy <jayfelix789@gmail.com>
2023-09-13 11:56:55 +0300

    added homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+
 function App() {
   return (
     <>
-      <div>hey</div>
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+      </Routes>
     </>
   );
 }
