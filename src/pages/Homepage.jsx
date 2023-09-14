0816ed4be1eba6b4bf7783159a8582de848d8eb8
irriz-jayy <jayfelix789@gmail.com>
2023-09-14 13:41:24 +0300

    added header section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,6 +4,12 @@ import Carousel from "../components/Carousel";
 const Homepage = () => {
   return (
     <div className="min-h-[100vh]">
+      <div className="h-[15vh] text-center p-2">
+        <p className="text-3xl">
+          Welcome to <span className="italic">Simply Good Recipes</span>!
+        </p>
+        <p className="text-orange-500">Your one stop for recipes</p>
+      </div>
       <div>
         <Carousel />
       </div>
