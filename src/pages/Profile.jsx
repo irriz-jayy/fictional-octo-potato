c76abccfd65235bb97a862a9aec1be40498ee8e1
irriz-jayy <jayfelix789@gmail.com>
2023-09-15 23:32:27 +0300

    minor change

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>This is my profile</div>;
+  return <div className="bg-orange-600">This is my profile</div>;
 };
 
 export default Profile;
