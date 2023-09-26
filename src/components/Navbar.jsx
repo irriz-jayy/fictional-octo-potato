261c05029a6b46c507bff400b32f641be4111073
irriz-jayy <jayfelix789@gmail.com>
2023-09-26 12:04:20 +0300

    added conditional rendering for our profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -149,19 +149,16 @@ export default function Navbar() {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
+                      {token && ( // Add this conditional check
+                        <Menu.Item>
                           <a
                             href="/profile"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
+                            className="block px-4 py-2 text-sm text-gray-700"
                           >
                             Your Profile
                           </a>
-                        )}
-                      </Menu.Item>
+                        </Menu.Item>
+                      )}
                       <Menu.Item>
                         {token ? (
                           <button
@@ -230,13 +227,15 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="mt-3 space-y-1">
-                <Disclosure.Button
-                  as="a"
-                  href="/profile"
-                  className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
-                >
-                  Your Profile
-                </Disclosure.Button>
+                {token && (
+                  <Disclosure.Button
+                    as="a"
+                    href="/profile"
+                    className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
+                  >
+                    Your Profile
+                  </Disclosure.Button>
+                )}
                 {token ? (
                   <Disclosure.Button
                     as="button"
