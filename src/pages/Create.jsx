aa35d0eb88bc10080d413639042614d446a13538
irriz-jayy <jayfelix789@gmail.com>
2023-09-27 22:52:50 +0300

    added create functionality(bugs)

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,9 +1,103 @@
 import { SendOutlined } from "@mui/icons-material";
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Create = () => {
+  const navigate = useNavigate();
+
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    country_of_origin: "",
+    description: "",
+    ingredients: "",
+    directions: "",
+    image_url: "",
+    number_of_people_served: "",
+    time: "",
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("http://127.0.0.1:3000/recipes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          resp.json().then((data) => {
+            // console.log(data);
+            // console.log("Success:", data);
+            toast.success("Recipe has been created successfully.");
+            navigate("/my_recipes");
+          });
+        } else {
+          // Handle errors
+          resp.json().then((errorData) => {
+            // console.error("Error:", errorData); // Log error response
+            toast.error(errorData);
+            // Handle errors
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        // Handle network errors
+      });
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      const formData = new FormData();
+      formData.append("file", file); // Use the 'file' object here
+      formData.append("upload_preset", "h9stgrub");
+
+      // Replace the Cloudinary URL with your own
+      fetch("https://api.cloudinary.com/v1_1/dm66wpmtb/image/upload", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // console.log("Cloudinary Success:", data);
+          // Extract the secure_url from the Cloudinary response
+          const imageUrl = data.secure_url;
+
+          // Update the profile_picture_url in formData
+          setFormData({
+            ...formData,
+            image_url: imageUrl,
+          });
+
+          // Optionally, you can also update the state for immediate display
+          setImageUrl(imageUrl);
+        })
+        .catch((error) => {
+          console.error("Error uploading image to Cloudinary:", error);
+        });
+    } else {
+      // Handle non-file input changes (e.g., text input)
+      // Handle non-file input changes (e.g., text input)
+      setFormData({
+        ...formData,
+        user: {
+          ...formData.user,
+          [name]: value, // Update the specific user attribute (e.g., name, email, etc.)
+        },
+      });
+    }
+  }
   return (
     <>
       <Navbar />
@@ -19,6 +113,8 @@ const Create = () => {
                 type="text"
                 className="rounded-md focus:border-orange-600 focus:ring-orange-600"
                 placeholder="Chapati"
+                value={formData.name}
+                onChange={handleChange}
               />
             </div>
             <div className="flex flex-col">
@@ -27,6 +123,8 @@ const Create = () => {
                 type="text"
                 className="rounded-md focus:border-orange-600 focus:ring-orange-600"
                 placeholder="Kenya"
+                value={formData.country_of_origin}
+                onChange={handleChange}
               ></input>
             </div>
             <div className="flex flex-col col-span-2">
@@ -36,6 +134,8 @@ const Create = () => {
                 rows={4}
                 className="rounded-md focus:border-orange-600 focus:ring-orange-600"
                 placeholder="Flat and round..."
+                value={formData.description}
+                onChange={handleChange}
               ></textarea>
             </div>
             <div className="flex flex-col col-span-2">
@@ -45,6 +145,8 @@ const Create = () => {
                 rows={4}
                 className="rounded-md focus:border-orange-600 focus:ring-orange-600"
                 placeholder="Flour, warm water..."
+                value={formData.ingredients}
+                onChange={handleChange}
               ></textarea>
             </div>
             <div className="flex flex-col col-span-2">
@@ -54,6 +156,8 @@ const Create = () => {
                 rows={4}
                 className="rounded-md focus:border-orange-600 focus:ring-orange-600"
                 placeholder="1. Knead the..."
+                value={formData.directions}
+                onChange={handleChange}
               ></textarea>
             </div>
             <div className="flex flex-col">
@@ -62,12 +166,15 @@ const Create = () => {
                 type="file"
                 // className="rounded-md"
                 accept="image/*"
+                onChange={handleChange}
               ></input>
             </div>
             <div className="flex flex-col">
               <label>Number of people served</label>
               <input
                 type="number"
+                value={formData.number_of_people_served}
+                onChange={handleChange}
                 className="rounded-md focus:border-orange-600 focus:ring-orange-600"
               ></input>
             </div>
@@ -77,9 +184,14 @@ const Create = () => {
                 type="text"
                 className="rounded-md focus:border-orange-600 focus:ring-orange-600"
                 placeholder="120 mins"
+                value={formData.time}
+                onChange={handleChange}
               ></input>
             </div>
-            <button className="bg-orange-600 col-span-2 h-12 text-white rounded-md hover:bg-orange-500">
+            <button
+              onClick={handleSubmit}
+              className="bg-orange-600 col-span-2 h-12 text-white rounded-md hover:bg-orange-500"
+            >
               Submit
               <span className="ml-2">
                 <SendOutlined />
