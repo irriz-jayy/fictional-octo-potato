ae7a587a58685067eea0f999b9f2eb9da4abeecc
irriz-jayy <jayfelix789@gmail.com>
2023-10-01 21:57:57 +0300

    updated the code

--- src/pages/MyRecipes.jsx
+++ src/pages/MyRecipes.jsx
@@ -9,32 +9,7 @@ import useUser from "../hooks/useUser";
 const MyRecipes = () => {
   const [recipes, setRecipes] = useState([]);
   const token = localStorage.getItem("token");
-  const [user, setUser] = useState([]);
-
-  useEffect(() => {
-    if (token) {
-      fetch("http://127.0.0.1:3000/profile", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error("Failed to fetch user data");
-          }
-        })
-        .then((userData) => {
-          setUser(userData);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-  }, [token]);
+  const user = useUser(token);
 
   console.log(user);
 
