87a4f32fc06eaf94d633354de80d5098012915c7
irriz-jayy <jayfelix789@gmail.com>
2023-10-02 14:18:39 +0300

    added save functionality for recipes

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -12,10 +12,50 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import useUser from "../hooks/useUser";
+import { toast } from "react-toastify";
 
 const FoodCard = ({ recipe }) => {
+  const [isSaved, setIsSaved] = useState(false); // State to track if the recipe is saved
+  const token = localStorage.getItem("token"); // Get the user's token
+  const user = useUser(token);
+
+  const handleSaveClick = () => {
+    // Check if the recipe is already saved (avoid duplicate saves)
+    if (isSaved) {
+      toast.info("Recipe is already saved.");
+      return;
+    }
+
+    // Make a POST request to add the recipe to bookmarks
+
+    if (!token) {
+      alert("Please log in to save recipes.");
+      return;
+    }
+
+    fetch(`http://127.0.0.1:3000/users/${user.id}/bookmarks`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ recipe_id: recipe.id }), // Send the recipe ID to save
+    })
+      .then((response) => {
+        if (response.ok) {
+          setIsSaved(true); // Update the state to indicate the recipe is saved
+          toast.success("Recipe saved successfully!");
+        } else {
+          throw new Error("Failed to save recipe.");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
     <>
       <Card sx={{ maxWidth: 300 }} elevation={3}>
@@ -60,7 +100,7 @@ const FoodCard = ({ recipe }) => {
               variant="contained"
               color="secondary"
               endIcon={<BookmarkAdd />}
-              onClick={() => alert("Added to bookmarks!")}
+              onClick={handleSaveClick}
             >
               Save
             </Button>
